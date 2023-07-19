f0a4135d0e1ab1e5b93a146038432084624034e2
Eric Wang <eric.wang401@gmail.com>
2023-07-19 04:23:10 +0000

    yes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,71 +6,12 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './theme.config.jsx'
   ],
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
-      animation: {
-        // Fade up and down
-        "fade-up": "fade-up 0.5s",
-        "fade-down": "fade-down 0.5s",
-        // Tooltip
-        "slide-up-fade": "slide-up-fade 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
-        "slide-down-fade": "slide-down-fade 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
-      },
-      keyframes: {
-        // Fade up and down
-        "fade-up": {
-          "0%": {
-            opacity: 0,
-            transform: "translateY(10px)",
-          },
-          "80%": {
-            opacity: 0.6,
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translateY(0px)",
-          },
-        },
-        "fade-down": {
-          "0%": {
-            opacity: 0,
-            transform: "translateY(-10px)",
-          },
-          "80%": {
-            opacity: 0.6,
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translateY(0px)",
-          },
-        },
-        // Tooltip
-        "slide-up-fade": {
-          "0%": { opacity: 0, transform: "translateY(6px)" },
-          "100%": { opacity: 1, transform: "translateY(0)" },
-        },
-        "slide-down-fade": {
-          "0%": { opacity: 0, transform: "translateY(-6px)" },
-          "100%": { opacity: 1, transform: "translateY(0)" },
-        },
-      },
-    },
+    extend: {}
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-    plugin(({ addVariant }) => {
-      addVariant("radix-side-top", '&[data-side="top"]')
-      addVariant("radix-side-bottom", '&[data-side="bottom"]')
-    }),
-  ],
+  plugins: [],
+  darkMode: 'class'
 }
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,8 @@
+import logo from '@/public/logo.svg'
+import Image from 'next/image'
+
 export default {
-    logo: <span>My Nextra Documentation</span>,
+    logo: <div className='flex items-center justify-center'><Image alt='Convoy logo' src={logo} className='w-6 h-6' /> <span className='ml-2 text-xl font-semibold'>Convoy</span></div>,
     project: {
         link: 'https://github.com/convoypanel/website'
     }
